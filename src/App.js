afb28ffd1afccbe860165c62c8de288f526c5f3b
StVu1991 <vukelic.stjepan@gmail.com>
2020-02-13 13:40:28 +0100

    Section 2 - React ToDo Task Part 1

--- src/App.js
+++ src/App.js
@@ -8,8 +8,35 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      userName : "Stjepan"
+      userName : "Stjepan",
+      todoItems : [
+        {action:"Buy a flowers", done:false},
+        {action:"Do workout", done:true},
+        {action:"Study React", done:false},
+        {action:"Call a friend", done:true}],
+        newToDoItemText:" "
     }
+  }
+
+  updateNewToDoItemText = (event) => {
+    this.setState({newToDoItemText : event.target.value});
+  }
+
+  createNewToDoTask = () => 
+  {
+    if (!this.state.todoItems.find(item => item.action === this.state.newToDoItemText))
+    {
+      this.setState ({
+        todoItems :  [...this.state.todoItems,
+            {action : this.state.newToDoItemText, done: false}], newToDoItemText : ""
+      });
+    }
+  }
+
+  changeStateData = () => {
+    this.setState(
+      {userName: this.state.userName === "Stjepan" ? "Dinosaur" : "Stjepan"}
+      )
   }
 
   render()
@@ -17,8 +44,18 @@ export default class App extends Component {
     return(
       <div>
         <h4 className="bg-primary text-white text-center p-2">
-          {this.state.userName} React Course
+          {this.state.userName} Todo List
+          ({this.state.todoItems.filter(t => !t.done).length}) items to do
         </h4>
+
+          <div className = "container-fluid">
+            <div className="m-1">
+              <input className="form-control" value={this.state.newToDoItemText} onChange={this.updateNewToDoItemText}></input>
+              <button className="btn btn-danger mt-1" onClick={this.createNewToDoTask}>
+                Add a new task
+              </button>
+            </div>
+          </div>
       </div>
     );
   }
